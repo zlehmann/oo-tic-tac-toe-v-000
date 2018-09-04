0c3fd53481ad0ba9fb0aaef50062b64c71c793f4
zlehmann@gmail.com <zlehmann@gmail.com>
2018-09-04 17:15:37 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,7 +3,7 @@ class TicTacToe
   def initialize(board=Array.new(9," "))
     @board=board
   end
-  
+
   WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
@@ -18,11 +18,11 @@ class TicTacToe
   def current_player()
     turn_count() % 2 == 0 ? "X" : "O"
   end
-   
+
   def turn_count()
     @board.count{|token| token == "X" || token == "O"}
   end
-   
+
   def display_board()
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
@@ -30,16 +30,16 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
-    
+
     def input_to_index(user_input)
       index = user_input.to_i - 1
       return index
     end
-    
+
     def move(index, token="X")
       @board[index] = token
     end
-    
+
     def position_taken?(index)
       if @board[index] == "X" || @board[index] == "O"
         return TRUE
@@ -47,7 +47,7 @@ class TicTacToe
         return FALSE
       end
     end
-    
+
     def valid_move?(position)
       if position.between?(0,8)
         if position_taken?(position)
@@ -59,7 +59,7 @@ class TicTacToe
         return FALSE
       end
     end
-    
+
     def turn()
       puts "Please enter 1-9:"
       input = gets.strip
@@ -72,7 +72,7 @@ class TicTacToe
         turn()
       end
     end
-    
+
     def won?()
       WIN_COMBINATIONS.each do |wincombo|
       if @board[wincombo[0]] == "X" && @board[wincombo[1]] == "X" && @board[wincombo[2]] == "X"
@@ -80,19 +80,19 @@ class TicTacToe
       elsif @board[wincombo[0]] == "O" && @board[wincombo[1]] == "O" && @board[wincombo[2]] == "O"
         return wincombo
       else
-        
+
       end
     end
     return false
   end
-  
+
   def full?()
     full_board = @board.none? do |index|
       index == " "
     end
     return full_board
   end
-  
+
   def draw?()
     if won?() != false
       return false
@@ -102,7 +102,7 @@ class TicTacToe
       return false
     end
   end
-  
+
   def over?()
     if won?() != false
       return true
@@ -112,7 +112,7 @@ class TicTacToe
       return false
     end
   end
-  
+
   def winner()
     WIN_COMBINATIONS.each do |wincombo|
       if @board[wincombo[0]] == "X" && @board[wincombo[1]] == "X" && @board[wincombo[2]] == "X"
@@ -120,12 +120,12 @@ class TicTacToe
       elsif @board[wincombo[0]] == "O" && @board[wincombo[1]] == "O" && @board[wincombo[2]] == "O"
         return "O"
       else
-      
+
       end
     end
     return nil
   end
-  
+
   def play()
     until over?() == true do
       turn()
